0adf90e64fdacda6c1729ac0b6d6973760204e09
Weißer Hase <wei3erHase@protonmail.com>
2023-08-03 16:30:48 +0200

    feat: script to update oracles (#3)
    * feat: main script without async
    
    * fix: async version
    
    * feat: improving oracles script
    
    * fix: deprecating getEnvVariable
    
    * fix: rm loop for txInProgress
    
    * fix: error in routine
    
    * feat: added pid-controller script
    
    * fix: linter
    
    ---------
    
    Co-authored-by: Dristpunk <dristpunk@gmail.com>

--- src/oracles.ts
+++ src/oracles.ts
@@ -1,7 +1,8 @@
-import type {Contract, Event} from 'ethers';
-import {providers, Wallet} from 'ethers';
+import { Bytes, Contract, utils } from 'ethers';
+import { providers, Wallet } from 'ethers';
 import dotenv from 'dotenv';
-import {getEnvVariable} from './utils/misc';
+import OracleJobAbi from './abis/OracleJob.json';
+import { env } from 'process';
 
 dotenv.config();
 
@@ -10,21 +11,22 @@ dotenv.config();
 /*============================================================== */
 
 // environment variables usage
-const provider = new providers.JsonRpcProvider(getEnvVariable('RPC_HTTPS_URI'));
-const txSigner = new Wallet(getEnvVariable('TX_SIGNER_PRIVATE_KEY'), provider);
+const provider = new providers.JsonRpcProvider(env.RPC_HTTPS_URI);
+const txSigner = new Wallet(env.TX_SIGNER_PRIVATE_KEY as any as Bytes, provider);
 
-const {oracleJob: job} = Contract;// TODO: declare oracleJob
+const oracleJob = new Contract('0xCC81b8E22Bc48133125BDa642C452EC6A52853C8', OracleJobAbi, txSigner);
 
-/**
- * chain = 420 (op goerli)
- * address = 0x140afB8dbdba2F7f47f45f9a4aa3142920BA5a20
- * abi = src/abis/OracleJob.json
- */
+const collateralTypes: string[] = [
+  utils.formatBytes32String('WETH'), // WETH
+  utils.formatBytes32String('OP'), // OP
+  utils.formatBytes32String('WBTC'), // WBTC
+  utils.formatBytes32String('STONES'), // STONES
+  utils.formatBytes32String('TOTEM'), // TOTEM
+];
 
 // Flag to track if there's a transaction in progress.
 // This is used to prevent multiple transactions from being sent at the same time.
-const txInProgress: Record<string, boolean> = {};
-
+let txInProgress: boolean;
 
 /* ==============================================================/*
                        MAIN SCRIPT
@@ -32,23 +34,57 @@ const txInProgress: Record<string, boolean> = {};
 
 export async function run(): Promise<void> {
   await Promise.all(
-    // TODO: create a list (hardcoded for now) of collateral types ['WETH','OP','WBTC','STONES','TOTEM']
-    // CollateralTypes are defined by bytes32('cType') in solidity, they should look like 0x574554480000
-    collateralTypes.map(async (event: Event) => {
-      await Promise.all(
-        /**
-         * try oracleJob.updateCollateralPrice(cType)
-         * if it works:
-         * - tx in progress: true
-         * - broadcast
-         */
-        broadcast
-      );
-    }),
+    collateralTypes.map(async (cType: string) => {
+      if (txInProgress) return;
+
+      let tx;
+      let gasUnits;
+      let gasPrice;
+      let txFee;
+      // emulate tx
+      try {
+        console.log(`Emulating: ${cType}`);
+        await oracleJob.callStatic.workUpdateCollateralPrice(cType);
+        txInProgress = true;
+
+        gasUnits = await oracleJob.estimateGas.workUpdateCollateralPrice(cType);
+        gasUnits = gasUnits.mul(12).div(10); // add 20% buffer
+        gasPrice = await provider.getGasPrice();
+        txFee = gasUnits.mul(gasPrice);
+        console.log(`Successfully emulated: ${cType}`);
+      } catch (err) {
+        txInProgress = false;
+        console.log(`Unsuccessfull emulation: ${cType}`);
+        return;
+      }
+
+      // check if we have enough funds for gas
+      const signerBalance = await provider.getBalance(txSigner.address);
+      if (signerBalance.lt(txFee)) {
+        console.log(`Insufficient balance for ${cType}`);
+        console.log(`Balance is ${utils.formatUnits(signerBalance, 'ether')}, but tx fee is ${utils.formatUnits(txFee, 'ether')}`);
+        txInProgress = false;
+        return;
+      }
+      // broadcast tx
+      try {
+        tx = await oracleJob.workUpdateCollateralPrice(cType, { gasLimit: gasUnits });
+        // wait for tx to be mined
+        await tx.wait();
+
+        console.log(`Successfully broadcasted: ${cType}`);
+      } catch (err) {
+        console.log(`Failed to broadcast: ${cType}`);
+        console.log(err);
+      }
+
+      txInProgress = false;
+    })
   );
 }
 
-
-(async () => {
+(async function main() {
+  console.log('Running...');
   await run();
+  setTimeout(main, 15 * 1000); // every 15 seconds
 })();
---
+++ src/pid-controller.ts
@@ -0,0 +1,78 @@
+import { Bytes, Contract, utils } from 'ethers';
+import { providers, Wallet } from 'ethers';
+import dotenv from 'dotenv';
+import OracleJobAbi from './abis/OracleJob.json';
+import { env } from 'process';
+
+dotenv.config();
+
+/* ==============================================================/*
+                          SETUP
+/*============================================================== */
+
+// environment variables usage
+const provider = new providers.JsonRpcProvider(env.RPC_HTTPS_URI);
+const txSigner = new Wallet(env.TX_SIGNER_PRIVATE_KEY as any as Bytes, provider);
+
+const oracleJob = new Contract('0xCC81b8E22Bc48133125BDa642C452EC6A52853C8', OracleJobAbi, txSigner);
+
+// Flag to track if there's a transaction in progress.
+// This is used to prevent multiple transactions from being sent at the same time.
+let txInProgress: boolean;
+
+/* ==============================================================/*
+                       MAIN SCRIPT
+/*============================================================== */
+
+export async function run(): Promise<void> {
+  if (txInProgress) return;
+
+  let tx;
+  let gasUnits;
+  let gasPrice;
+  let txFee;
+  // emulate tx
+  try {
+    console.log(`Emulating: PID Controller`);
+    await oracleJob.callStatic.workUpdateRate();
+    txInProgress = true;
+
+    gasUnits = await oracleJob.estimateGas.workUpdateRate();
+    gasUnits = gasUnits.mul(12).div(10); // add 20% buffer
+    gasPrice = await provider.getGasPrice();
+    txFee = gasUnits.mul(gasPrice);
+    console.log(`Successfully emulated: PID Controller`);
+  } catch (err) {
+    txInProgress = false;
+    console.log(`Unsuccessfull emulation: PID Controller`);
+    return;
+  }
+
+  // check if we have enough funds for gas
+  const signerBalance = await provider.getBalance(txSigner.address);
+  if (signerBalance.lt(txFee)) {
+    console.log(`Insufficient balance for PID Controller`);
+    console.log(`Balance is ${utils.formatUnits(signerBalance, 'ether')}, but tx fee is ${utils.formatUnits(txFee, 'ether')}`);
+    txInProgress = false;
+    return;
+  }
+  // broadcast tx
+  try {
+    tx = await oracleJob.workUpdateRate({ gasLimit: gasUnits });
+    // wait for tx to be mined
+    await tx.wait();
+
+    console.log(`Successfully broadcasted: PID Controller`);
+  } catch (err) {
+    console.log(`Failed to broadcast: PID Controller`);
+    console.log(err);
+  }
+
+  txInProgress = false;
+}
+
+(async function main() {
+  console.log('Running...');
+  await run();
+  setTimeout(main, 15 * 1000); // every 15 seconds
+})();
